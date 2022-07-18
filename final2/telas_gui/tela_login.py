e76174d4954e61a62de90c52a1d5d07d627a3721
felipeabe <105646416+felipeabe@users.noreply.github.com>
2022-07-18 14:02:25 -0300

    Update tela_login.py

--- final2/telas_gui/tela_login.py
+++ final2/telas_gui/tela_login.py
@@ -3,21 +3,9 @@ import PySimpleGUI as sg
 class TelaLogin:
     def __init__(self):
         self.__janela=None
-        self.menu()
+
 
     def dados_login(self):
-        self.menu()
-        evento,valores=self.__janela.Read()
-        #NÃO ESTA FUNCIONANDO
-        if evento=='sair' or sg.WIN_CLOSED:
-            self.fechar()
-
-        cadastro={'usuario': valores['usuario'],'idpokedex': valores['idpokedex']}
-        self.fechar()
-        return cadastro
-
-
-    def menu(self):
         layout=[
             [sg.Text('Usuario',size=10),sg.Input(key='usuario')],
             [sg.Text('IDpokedex',size=10),sg.Input(key='idpokedex')],
@@ -25,9 +13,19 @@ class TelaLogin:
         ]
 
         self.__janela=sg.Window('Tela de login').layout(layout)
+        evento,valores=self.abrir()
+        if evento=='sair' or sg.WIN_CLOSED:
+            self.fechar()
+        usuario=valores['usuario']
+        idpokdedex=valores['idpokedex']
+        self.fechar()
+        return{'usuario':usuario,'idpokedex':idpokdedex}
 
 
     def fechar(self):
         self.__janela.Close()
 
 
+    def abrir(self):
+        evento, valores = self.__janela.Read()
+        return evento, valores
