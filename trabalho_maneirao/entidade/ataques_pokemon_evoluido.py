621a6bd77efe2dcc134a5cbc5de47327ba1c3e99
felipeabe <105646416+felipeabe@users.noreply.github.com>
2023-03-29 10:55:31 -0300

    Delete trabalho_maneirao directory

--- trabalho_maneirao/entidade/ataques_pokemon_evoluido.py
+++
@@ -1,8 +0,0 @@
-# from trabalho_maneirao.entidade.ataques_pokemon import AtaquesPokemon
-#
-# class AtaquesPokemonEvoluido(AtaquesPokemon):
-#     def __init__(self,ataque1_nome,ataque1_valor):
-#         super().__init__()
-#         print(ataque1_nome)
-#
-# a=AtaquesPokemonEvoluido
